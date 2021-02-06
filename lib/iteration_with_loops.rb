093d5aad7906e0534c887f3694070ab79c0d8358
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-06 16:07:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    
+    #binding.pry
     if src[row_index][element_index].is_a? = =true
       nested_strings<<src[row_index][element_index]
     end
