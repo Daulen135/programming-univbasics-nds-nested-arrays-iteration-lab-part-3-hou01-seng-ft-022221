4685a470bef316a7a72d721217ba0a7cf1c1b84c
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-06 15:58:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,6 +9,8 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
+    if src[row_index][element_index].is_a?==true
+    
     total += src[row_index][element_index]
     element_index += 1
   end
